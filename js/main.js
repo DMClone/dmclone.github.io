b4eb50cc37943b62286f05de57f2e5a67ff63518
DMClone <230381@student.glu.nl>
2025-09-03 13:29:54 +0200

    Much more content, github/itchio buttons, snippets for description

--- js/main.js
+++ js/main.js
@@ -20,9 +20,9 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-const gluDiv = document.querySelector(".gludiv");
-const gluetcDiv = document.querySelector(".gluetcdiv");
-const rocDiv = document.querySelector(".rocdiv");
+const blockonediv = document.querySelector(".blockonediv");
+const blocktwodiv = document.querySelector(".blocktwodiv");
+const blockthreediv = document.querySelector(".blockthreediv");
 
 var jsonData;
 
@@ -30,56 +30,107 @@ fetch("../json/projects.json")
   .then((response) => response.json())
   .then((data) => {
     jsonData = data;
-    for (A = 0; A <= jsonData.glu.length; A++) {
-      const useData = jsonData.glu[A];
+    for (A = 0; A <= jsonData.blockone.length; A++) {
+      const useData = jsonData.blockone[A];
+
       if (useData != undefined) {
-        gluDiv.innerHTML += `<div class="card d-flex flex-column">
-                <img class="card-img-top" src="${useData.img}" alt="${useData.altText}">
+        let itchioButton = ItchioLink(useData.itchio);
+        let githubButton = GithubLink(useData.github);
+
+        blockonediv.innerHTML += `<div class="card d-flex flex-column">
+                <img class="card-img-top" src="${useData.thumbnail}" alt="${useData.altText}">
                 <div class="card-body flex-grow-1">
                     <h4 class="card-title">${useData.title}</h4>
                     <p class="card-text">${useData.description}</p>
                 </div>
                 <div class="card-footer">
                     <small class="text-body-secondary">
-                        <button class="btn btn-primary card-button" onclick="OpenOverlay('glu', '${A}')">View
-                            More</button></small>
+                    ${itchioButton}
+                        <button class="btn btn-primary card-button" onclick="OpenOverlay('blockone', '${A}')">View
+                            More</button>
+                            ${githubButton}
+                            </small>
                 </div>
             </div>`;
       }
     }
 
-    for (A = 0; A <= jsonData.gluetc.length; A++) {
-      const useData = jsonData.gluetc[A];
+    for (A = 0; A <= jsonData.blocktwo.length; A++) {
+      const useData = jsonData.blocktwo[A];
       if (useData != undefined) {
-        gluetcDiv.innerHTML += `<div class="card d-flex flex-column">
-                <img class="card-img-top" src="${useData.img}" alt="${useData.altText}">
+        let itchioButton = ItchioLink(useData.itchio);
+        let githubButton = GithubLink(useData.github);
+
+        blocktwodiv.innerHTML += `<div class="card d-flex flex-column">
+                <img class="card-img-top" src="${useData.thumbnail}" alt="${useData.altText}">
                 <div class="card-body flex-grow-1">
                     <h4 class="card-title">${useData.title}</h4>
                     <p class="card-text">${useData.description}</p>
                 </div>
                 <div class="card-footer">
-                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    <small class="text-body-secondary">
+                    ${itchioButton}
+                        <button class="btn btn-primary card-button" onclick="OpenOverlay('blocktwo', '${A}')">View
+                            More</button>
+                            ${githubButton}
+                            </small>
                 </div>
             </div>`;
       }
     }
 
-    for (A = 0; A <= jsonData.roc.length; A++) {
-      const useData = jsonData.roc[A];
+    for (A = 0; A <= jsonData.blockthree.length; A++) {
+      const useData = jsonData.blockthree[A];
       if (useData != undefined) {
-        rocDiv.innerHTML += `<div class="card d-flex flex-column">
-                <img class="card-img-top" src="${useData.img}" alt="${useData.altText}">
+        let itchioButton = ItchioLink(useData.itchio);
+        let githubButton = GithubLink(useData.github);
+
+        blockthreediv.innerHTML += `<div class="card d-flex flex-column">
+                <img class="card-img-top" src="${useData.thumbnail}" alt="${useData.altText}">
                 <div class="card-body flex-grow-1">
                     <h4 class="card-title">${useData.title}</h4>
                     <p class="card-text">${useData.description}</p>
                 </div>
                 <div class="card-footer">
-                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    <small class="text-body-secondary">
+                    ${itchioButton}
+                        <button class="btn btn-primary card-button" onclick="OpenOverlay('blockthree', '${A}')">View
+                            More</button>
+                            ${githubButton}
+                            </small>
                 </div>
             </div>`;
       }
     }
   });
+
+function ItchioLink(itchio) {
+  switch (itchio) {
+    case "soon":
+      itchio = `<button class="btn btn-secondary card-button" disabled>Itch.io (Soon)</button>`;
+      break;
+    case "":
+      break;
+    default:
+      itchio = `<a href="${itchio}" target="_blank" class="btn btn-secondary card-button">Itch.io</a>`;
+      break;
+  }
+  return itchio;
+}
+
+function GithubLink(github) {
+  switch (github) {
+    case "soon":
+      github = `<button class="btn btn-secondary card-button" disabled>Itch.io (Soon)</button>`;
+      break;
+    case "":
+      break;
+    default:
+      github = `<a href="${github}" target="_blank" class="btn btn-secondary card-button">Github</a>`;
+      break;
+  }
+  return github;
+}
 
 var overlay = document.getElementById("overlay");
 
@@ -95,7 +146,7 @@ function OpenOverlay(key, index) {
 function FillOverlay(key, index) {
   const useData = jsonData[key][index];
   var descriptions = useData.extendedDescriptions;
-  var codeSnippets = useData.code_snippets;
+  var images = useData.images;
   if (useData != undefined) {
     overlay.innerHTML = `<div class="overlay-card">
                 <div class="overlay-card-body">
@@ -111,8 +162,8 @@ function FillOverlay(key, index) {
       if (index < descriptions.length) {
         cardText.innerHTML += `<p>${descriptions[index]}</p>`;
       }
-      if (index < codeSnippets.length) {
-        cardText.innerHTML += `<div class="coolcode"><pre><code>${codeSnippets[index]}</code></pre></div>`;
+      if (images[index] != "" && index < images.length) {
+        cardText.innerHTML += `<image class="card-img-top" src= "${images[index]}"><image>`;
       }
     }
   }
